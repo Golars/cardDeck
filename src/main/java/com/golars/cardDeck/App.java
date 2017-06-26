45303b206a2c3a0e1a0bee1f5ee943b112f81e9e
Golars <titorens@gmail.com>
2017-06-26 17:47:15 +0300

    [+] init, travic

---
+++ src/main/java/com/golars/cardDeck/App.java
@@ -0,0 +1,13 @@
+package com.golars.cardDeck;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Card Deck!" );
+    }
+}
